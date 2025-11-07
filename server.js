891c08dc8339549a7e2cc734331893448012512e
quitel-hub <karpenko.k.a.07@gmail.com>
2025-11-07 23:29:11 +0100

    change email

--- server.js
+++ server.js
@@ -928,7 +928,7 @@ const ownerVerifySchema = z.object({
 
 // Жестко заданные данные администратора - максимально сложные для безопасности
 const ADMIN_CREDENTIALS = {
-  email: "karpenko.k.a.07@gmail.com",
+  email: "sushi.icon.rolles.nl@gmail.com",
   accessCode: "SUSHI-MASTER-2024-X9K7",
   password: "SushiMaster2024!@#$%^&*()_+{}|:<>?[];',./",
   name: "Главный администратор"
